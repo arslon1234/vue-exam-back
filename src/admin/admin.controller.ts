8131bb8fb337f79bd524ab19a5f71e7826de1cf7
valiev1011 <valievyunus1011@gmail.com>
2023-12-07 21:32:01 +0500

    login add

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -16,14 +16,15 @@ import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
 import { Request, Response } from 'express';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiProperty } from '@nestjs/swagger';
+import { LoginAdminDto } from './dto/login-admin.dto';
 
 @Controller('admin')
-
 @ApiBearerAuth()
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
+  @ApiProperty({ example: 'John Doe', description: 'Admin full name' })
   @Post()
   create(
     @Body() createAdminDto: CreateAdminDto,
@@ -31,6 +32,14 @@ export class AdminController {
     @Req() req: Request,
   ) {
     return this.adminService.create(createAdminDto, res, req);
+  }
+
+  @Post('signin')
+  signin(
+    @Body() loginAdminDto: LoginAdminDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    return this.adminService.signin(loginAdminDto, res);
   }
 
   @Get()
